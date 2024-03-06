50f435fef2d0f7b8ee92f5eaf86a6b92a938e363
Victor Azevedo <josevcazevedo@gmail.com>
2024-03-06 09:32:56 -0300

    feat: add GET /health

--- DotPix/Program.cs
+++ DotPix/Program.cs
@@ -1,7 +1,12 @@
+using DotPix.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
+
+builder.Services.AddScoped<HealthService>();
+
 builder.Services.AddEndpointsApiExplorer();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
