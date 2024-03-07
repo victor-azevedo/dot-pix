abc8fdb22953bf703c8ca4533b0c43fd0728c7ba
Victor Azevedo <josevcazevedo@gmail.com>
2024-03-06 23:16:07 -0300

    chore: fix PaymentProviderTokenEntity migration

---
+++ DotPix/Migrations/20240307021233_FixPaymentProviderTokenEntity.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DotPix.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixPaymentProviderTokenEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
