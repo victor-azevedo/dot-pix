ac7855df2ba6f91898b452f81ca05a7332c9a740
Victor Azevedo <josevcazevedo@gmail.com>
2024-03-06 22:04:41 -0300

    refactor: add User primary constructor

--- DotPix/Models/User.cs
+++ DotPix/Models/User.cs
@@ -4,32 +4,24 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace DotPix.Models;
 
 [Table("users")]
-public class User
+public class User(string name, string cpf)
 {
-    
     [Key]
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     [Column("id")]
     public int Id { get; set; }
-    
-    [Required]
-    [Column("name",TypeName = "varchar(255)")]
-    public string Name { get; set; }
 
     [Required]
-    [Column("cpf",TypeName = "varchar(11)")]
-    public string Cpf { get; set; }
+    [Column("name", TypeName = "varchar(255)")]
+    public string Name { get; set; } = name;
+
+    [Required]
+    [Column("cpf", TypeName = "varchar(11)")]
+    public string Cpf { get; set; } = cpf;
 
     [Column("created_at")]
-    public DateTime CreatedAt { get; init; }
-    
-    [Column("updated_at")]
-    public DateTime UpdatedAt { get; set; }
+    public DateTime CreatedAt { get; init; } = DateTime.UtcNow;
 
-    public User(string name, string cpf)
-    {
-        Name = name;
-        Cpf = cpf;
-        CreatedAt = UpdatedAt = DateTime.UtcNow;
-    }
+    [Column("updated_at")]
+    public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
 }
