ac60b7fd21cdf79b7b65082c34e3726b553e9538
Victor Azevedo <josevcazevedo@gmail.com>
2024-03-06 23:45:56 -0300

    chore: add PixKeyEntity migration

---
+++ DotPix/Migrations/20240307024039_AddPixKeyEntity.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace DotPix.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPixKeyEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "pix_keys",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    type = table.Column<string>(type: "varchar(10)", nullable: false),
+                    value = table.Column<string>(type: "varchar(255)", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    updated_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    payment_provider_account_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_pix_keys", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_pix_keys_payment_provider_accounts_payment_provider_account~",
+                        column: x => x.payment_provider_account_id,
+                        principalTable: "payment_provider_accounts",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_pix_keys_payment_provider_account_id",
+                table: "pix_keys",
+                column: "payment_provider_account_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "pix_keys");
+        }
+    }
+}
