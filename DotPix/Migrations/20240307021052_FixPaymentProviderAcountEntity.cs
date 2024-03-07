9dacef30e19962c6762229ac1e8245fe4c748651
Victor Azevedo <josevcazevedo@gmail.com>
2024-03-06 23:15:53 -0300

    chore: fix PaymentProviderAccountEntity migration

---
+++ DotPix/Migrations/20240307021052_FixPaymentProviderAcountEntity.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace DotPix.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixPaymentProviderAcountEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "payment_provider_accounts",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    account = table.Column<string>(type: "varchar(20)", nullable: false),
+                    agency = table.Column<string>(type: "varchar(20)", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    updated_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    payment_provider_id = table.Column<int>(type: "integer", nullable: false),
+                    user_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_payment_provider_accounts", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_payment_provider_accounts_payment_providers_payment_provide~",
+                        column: x => x.payment_provider_id,
+                        principalTable: "payment_providers",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_payment_provider_accounts_users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_payment_provider_accounts_payment_provider_id",
+                table: "payment_provider_accounts",
+                column: "payment_provider_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_payment_provider_accounts_user_id",
+                table: "payment_provider_accounts",
+                column: "user_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "payment_provider_accounts");
+        }
+    }
+}
