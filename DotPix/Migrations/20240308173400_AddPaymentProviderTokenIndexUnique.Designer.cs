0429343a0e20f995c4d6c444d29d3da37128bac6
Victor Azevedo <josevcazevedo@gmail.com>
2024-03-09 16:03:06 -0300

    feat: add migrations index, unique in db tables

--- DotPix/Migrations/20240308173400_AddPaymentProviderTokenIndexUnique.Designer.cs
+++ DotPix/Migrations/20240308173400_AddPaymentProviderTokenIndexUnique.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using DotPix.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace DotPix.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240308173400_AddPaymentProviderTokenIndexUnique")]
+    partial class AddPaymentProviderTokenIndexUnique
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -196,9 +199,6 @@ namespace DotPix.Migrations
                         .HasColumnName("updated_at");
 
                     b.HasKey("Id");
-
-                    b.HasIndex("Cpf")
-                        .IsUnique();
 
                     b.ToTable("users");
                 });
---
+++ DotPix/Migrations/20240309040132_UpdatePaymentProviderAccoun.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DotPix.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatePaymentProviderAccoun : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_users_cpf",
+                table: "users",
+                column: "cpf",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_users_cpf",
+                table: "users");
+        }
+    }
+}
