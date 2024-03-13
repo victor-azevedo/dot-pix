08d8d40e4b727592c005be6cad3786c6f5a0632f
Victor Azevedo <josevcazevedo@gmail.com>
2024-03-13 10:33:22 -0300

    refactor: script postKeysTest

--- Tests/.k6/postKeysTest.js
+++ Tests/.k6/postKeysTest.js
@@ -12,7 +12,8 @@ import {
 const ACCOUNTS_FILE_PATH = "../Mocks/accounts.json";
 const KEYS_FILE_PATH = "../Mocks/keys.json";
 
-const payloads = generatePayloads();
+const payloads = postKeyPayloadsGenerator(PAYLOAD_LENGTH);
+
 export const options = defaultOptions;
 export default function () {
     const url = `${API_URL}/keys`;
@@ -26,48 +27,51 @@ export default function () {
     const payload = JSON.stringify(getRandomElement(payloads));
 
     http.post(url, payload, params);
+
 }
 
-
-function generatePayloads() {
-    console.log("Creating payloads...")
+function postKeyPayloadsGenerator(length) {
+    console.log("Creating payloads...");
     const accounts = parseJsonToArray("accounts", ACCOUNTS_FILE_PATH);
     const keys = parseJsonToArray("keys", KEYS_FILE_PATH);
 
-    if (accounts.length < PAYLOAD_LENGTH || keys.length < PAYLOAD_LENGTH || usersSeed.length < PAYLOAD_LENGTH)
+    if (accounts.length < length || keys.length < length || usersSeed.length < length)
         throw new Error('Small mock`s files');
 
     const payloads = []
-    for (let i = 0; i < PAYLOAD_LENGTH; i++) {
+    for (let i = 0; i < length; i++) {
         const key = getRandomElement(keys);
         const account = getRandomElement(accounts);
         const user = getRandomElement(usersSeed);
-        payloads.push({
-                "key":
-                    {
-                        "value":
-                            key["value"],
-                        "type":
-                            key["type"]
-                    }
-                ,
-                "user":
-                    {
-                        "cpf":
-                            user["Cpf"]
-                    }
-                ,
-                "account":
-                    {
-                        "number":
-                            account["number"],
-                        "agency":
-                            account["agency"]
-                    }
-            }
-        )
+        payloads.push(postKeysPayloadParse({key, account, user}));
     }
 
     console.log("Payloads created successfully!")
     return payloads;
+}
+
+function postKeysPayloadParse({key, account, user}) {
+    return {
+        "key":
+            {
+                "value":
+                    key["value"],
+                "type":
+                    key["type"]
+            }
+        ,
+        "user":
+            {
+                "cpf":
+                    user["Cpf"]
+            }
+        ,
+        "account":
+            {
+                "number":
+                    account["number"],
+                "agency":
+                    account["agency"]
+            }
+    }
 }
