845242253e40295be38d2b29d2a4556815c89592
Victor Azevedo <josevcazevedo@gmail.com>
2024-03-15 15:27:58 -0300

    chore: add Dockerfile for WORKER

--- DotPixWorker/Services/ConsumerPaymentQueue.cs
+++ DotPixWorker/Services/ConsumerPaymentQueue.cs
@@ -1,16 +1,21 @@
 using DotPixWorker.Interfaces;
 using System.Text;
+using Microsoft.Extensions.Options;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 
 
 namespace DotPixWorker.Services;
 
-public class ConsumerPaymentQueue(ILogger<ConsumerPaymentQueue> logger) : IConsumerPaymentQueue
+public class ConsumerPaymentQueue(IOptions<AppParameters> options, ILogger<ConsumerPaymentQueue> logger)
+    : IConsumerPaymentQueue
 {
     public void WatchReceive(CancellationToken stoppingToken)
     {
-        var factory = new ConnectionFactory { HostName = "localhost" };
+        var factory = new ConnectionFactory
+        {
+            HostName = options.Value.RabbitMq.HostName
+        };
         using var connection = factory.CreateConnection();
         using var channel = connection.CreateModel();
 
@@ -35,6 +40,8 @@ public class ConsumerPaymentQueue(ILogger<ConsumerPaymentQueue> logger) : IConsu
             consumer: consumer);
 
         logger.LogInformation(" Press \"Ctrl+C\" to exit.");
+
+        Thread.Sleep(2000);
         while (!stoppingToken.IsCancellationRequested)
         {
         }
