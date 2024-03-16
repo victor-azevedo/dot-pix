a1719fe14c8472880caed21e8586e9ebd89702ac
Victor Azevedo <josevcazevedo@gmail.com>
2024-03-16 11:44:48 -0300

    chore: add migration AddApiUrlToPaymentProviders

---
+++ DotPixApi/Migrations/20240316143640_AddApiUrlToPaymentProviders.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DotPixApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddApiUrlToPaymentProviders : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "api_url",
+                table: "payment_providers",
+                type: "varchar(255)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "api_url",
+                table: "payment_providers");
+        }
+    }
+}
