550cfc15cac2eb4f1ecda809de129b79afbf5d51
Victor Azevedo <josevcazevedo@gmail.com>
2024-03-16 20:24:58 -0300

    chore: add WorkerName environment variable

--- DotPixWorker/AppParameters.cs
+++ DotPixWorker/AppParameters.cs
@@ -3,15 +3,10 @@ namespace DotPixWorker;
 public class AppParameters
 {
     public RabbitMQ RabbitMq { get; set; }
-    public Database Database { get; set; }
+    public string WorkerName { get; set; }
 }
 
 public class RabbitMQ
 {
     public string HostName { get; set; }
-}
-
-public class Database
-{
-    public string Dotpix { get; set; }
 }
