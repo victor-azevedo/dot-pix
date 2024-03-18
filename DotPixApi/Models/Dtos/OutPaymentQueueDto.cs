96f98ddf84b2216e473e30f38748d846faecae29
Victor Azevedo <josevcazevedo@gmail.com>
2024-03-18 16:43:43 -0300

    feat: add status and expireAt to OutPaymentQueueDto

--- DotPixApi/Models/Dtos/OutPaymentQueueDto.cs
+++ DotPixApi/Models/Dtos/OutPaymentQueueDto.cs
@@ -1,10 +1,12 @@
 namespace DotPixApi.Models.Dtos;
 
-public class OutPaymentQueueDto(Payments payment)
+public class OutPaymentQueueDto(Payments payment, DateTime expireAt)
 {
     public Guid PaymentId { get; set; } = payment.Id;
     public int Amount { get; set; } = payment.Amount;
     public string? Description { get; set; } = payment.Description;
     public OutPaymentOriginDto Origin { get; set; } = new(payment.AccountOrigin);
     public OutPaymentDestinyQueueDto Destiny { get; set; } = new(payment.PixKeyDestiny);
+    public PaymentStatus Status { get; set; } = payment.Status;
+    public DateTime ExpireAt { get; set; } = expireAt;
 }
