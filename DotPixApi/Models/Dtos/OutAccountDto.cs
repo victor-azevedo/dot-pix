1bcad85d699dd7281449bb1d595a349eadc0f987
Victor Azevedo <josevcazevedo@gmail.com>
2024-03-18 16:42:53 -0300

    refactor: change BankId type

--- DotPixApi/Models/Dtos/OutAccountDto.cs
+++ DotPixApi/Models/Dtos/OutAccountDto.cs
@@ -4,5 +4,5 @@ public class OutAccountDto(PaymentProviderAccount paymentProviderAccount)
 {
     public string Number { get; set; } = paymentProviderAccount.Account;
     public string Agency { get; set; } = paymentProviderAccount.Agency;
-    public string BankId { get; set; } = paymentProviderAccount.PaymentProviderId.ToString();
+    public int BankId { get; set; } = paymentProviderAccount.PaymentProviderId;
 }
