1ea85eb149c15fb9ab514c9665a428db4d3e41c4
Victor Azevedo <josevcazevedo@gmail.com>
2024-03-23 15:35:32 -0300

    refactor: RabbitMq from IConnectionFactory to IModel channel injection

--- DotPixConciliationWorker/Program.cs
+++ DotPixConciliationWorker/Program.cs
@@ -1,5 +1,6 @@
 using DotPixConciliationWorker;
 using DotPixConciliationWorker.Data;
+using DotPixConciliationWorker.Exceptions;
 using DotPixConciliationWorker.Interfaces;
 using DotPixConciliationWorker.Options;
 using DotPixConciliationWorker.Services;
@@ -8,28 +9,48 @@ using RabbitMQ.Client;
 
 var builder = Host.CreateApplicationBuilder(args);
 
-// Database
-builder.Services.AddDbContextFactory<DotPixDbContext>();
-
 // Environment variables config
 IConfiguration config = builder.Configuration;
 builder.Services.Configure<AppParameters>(config.GetSection("AppParameters"));
 
+// Database
+builder.Services.AddDbContextFactory<DotPixDbContext>();
+
 // Message Broker
 builder.Services.Configure<RabbitMqOptions>(builder.Configuration.GetSection("AppParameters:RabbitMq"));
-builder.Services.AddSingleton<IConnectionFactory>(serviceProvider =>
+builder.Services.AddSingleton<IModel>(serviceProvider =>
 {
     var options = serviceProvider.GetRequiredService<IOptions<RabbitMqOptions>>().Value;
-    return new ConnectionFactory() { HostName = options.HostName };
+    var connectionFactory = new ConnectionFactory { HostName = options.HostName };
+    var connection = connectionFactory.CreateConnection();
+    return connection.CreateModel();
 });
 
-// Services
-builder.Services.AddHostedService<ConciliationWorker>();
+// RabbitMq Consumer
+builder.Services.AddSingleton<IQueueConsumerService<ConciliationMessageProcessor>,
+    RabbitMqConsumerService<ConciliationMessageProcessor>>();
+builder.Services.AddSingleton<IMessageProcessor<ConciliationMessageProcessor>,
+    ConciliationMessageProcessor>();
 
-builder.Services.AddSingleton<IMessageProcessor<ConciliationMessageProcessor>, ConciliationMessageProcessor>();
-builder.Services
-    .AddSingleton<IQueueConsumerService<ConciliationMessageProcessor>,
-        RabbitMqConsumerService<ConciliationMessageProcessor>>();
+builder.Services.AddSingleton<IQueueConsumerService<ConciliationQueueExceptions>,
+    RabbitMqConsumerService<ConciliationQueueExceptions>>();
+builder.Services.AddSingleton<IQueueExceptions<ConciliationQueueExceptions>,
+    ConciliationQueueExceptions>();
+
+// PSP Client
+builder.Services.AddHttpClient<PspClient>();
+builder.Services.AddSingleton<IPspClient, PspClient>(serviceProvider =>
+{
+    var logger = serviceProvider.GetRequiredService<ILogger<PspClient>>();
+    return new PspClient(logger);
+});
+
+// File Services 
+builder.Services.AddSingleton<IConciliationFileProcessor, ConciliationFileProcessor>();
+builder.Services.AddSingleton<IFileReader, NdjsonReader>();
+
+// Worker
+builder.Services.AddHostedService<ConciliationWorker>();
 
 var host = builder.Build();
 host.Run();
--- DotPixConciliationWorker/Services/RabbitMqConsumerService.cs
+++ DotPixConciliationWorker/Services/RabbitMqConsumerService.cs
@@ -5,32 +5,32 @@ using RabbitMQ.Client.Events;
 namespace DotPixConciliationWorker.Services;
 
 public class RabbitMqConsumerService<TMessageProcessor>(
-    IConnectionFactory factory,
+    IModel channel,
     IMessageProcessor<ConciliationMessageProcessor> messageProcessor,
     ILogger<RabbitMqConsumerService<ConciliationMessageProcessor>> logger)
     : IQueueConsumerService<TMessageProcessor>
 {
     public async Task ConsumeMessage(string queueName, CancellationToken stoppingToken)
     {
-        using var connection = factory.CreateConnection();
-        using var channel = connection.CreateModel();
+        using (channel)
+        {
+            DeclareQueue(queueName);
 
-        DeclareQueue(channel, queueName);
+            var consumer = new EventingBasicConsumer(channel);
 
-        var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += async (model, ea) => await messageProcessor.ProcessMessageAsync(model, ea, channel);
 
-        consumer.Received += async (model, ea) => await messageProcessor.ProcessMessageAsync(model, ea, channel);
+            channel.BasicConsume(queue: "conciliation", autoAck: false, consumer: consumer);
 
-        channel.BasicConsume(queue: "conciliation", autoAck: false, consumer: consumer);
+            logger.LogInformation(" Press \"Ctrl+C\" to exit.");
 
-        logger.LogInformation(" Press \"Ctrl+C\" to exit.");
+            logger.LogInformation($"Started consuming messages from queue '{queueName}'");
 
-        logger.LogInformation($"Started consuming messages from queue '{queueName}'");
-
-        await Task.Delay(Timeout.Infinite, stoppingToken);
+            await Task.Delay(Timeout.Infinite, stoppingToken);
+        }
     }
 
-    private void DeclareQueue(IModel channel, string queueName)
+    private void DeclareQueue(string queueName)
     {
         channel.QueueDeclare(
             queue: queueName,
